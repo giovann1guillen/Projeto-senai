bd05312e3cf0d5839a98c8a987f9bad782646e4d
giovann1guillen <105879909+giovann1guillen@users.noreply.github.com>
2022-08-01 17:00:52 -0300

    registro

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿using Projeto_senai.Model;
-
-Usuario usuario = new Usuario();
-usuario.Nome = "Rogerinho";
-usuario.Senha = "senha1234";
+﻿using Projeto_Senai.Model;
 
 ExemploSenaiContext context = new ExemploSenaiContext();
-context.Add(usuario);
-context.SaveChanges();
+foreach (var usuario in context.Usuarios)
+{
+    Console.WriteLine(usuario.Nome);
+}
+
+while (True)
+(
+    Console.WriteLine("o que deseja fazer? 1 - Logar" +  
+    "2 - registrar" + "3 - sair");
+    string num = Console.Readline();
+    if (num == "1")
+    (
+        Console.Clear();
+    )
+    else if (num == "2")
+    (
+        Console.Clear();
+    )
+    else if (num == "3")
+    (
+       break;
+    )
+    else
+    (
+        Console.Clear();
+        Console.WriteLine("Operação invalida");
+    )
+)
