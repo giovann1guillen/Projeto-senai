264b68cd0190d791838c54fb720e15098d6c2cff
giovann1guillen <105879909+giovann1guillen@users.noreply.github.com>
2022-08-03 15:44:32 -0300

    registro de pessoas

--- Program.cs
+++ Program.cs
@@ -1,31 +1,49 @@
-﻿using Projeto_Senai.Model;
+﻿using Projeto_senai.Model;
 
 ExemploSenaiContext context = new ExemploSenaiContext();
-foreach (var usuario in context.Usuarios)
+while (true)
 {
-    Console.WriteLine(usuario.Nome);
-}
-
-while (True)
-(
-    Console.WriteLine("o que deseja fazer? 1 - Logar" +  
-    "2 - registrar" + "3 - sair");
+    Console.WriteLine("o que deseja fazer? 1 - Logar ou 2 - registrar ou 3 - sair");
     string num = Console.Readline();
     if (num == "1")
-    (
-        Console.Clear();
-    )
+    {
+        Console.WriteLine("digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        Usuario expectedUser = null;
+        foreach(Usuario usuario in db.usuario)
+        {
+            if(usuario.Nome == nome)
+            {
+                expectedUser = usuario;
+            }
+            if(expectedUser== null)
+            {
+                Console.WriteLine("Não existe usuario com esse nome");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+        } 
+       
+    }
     else if (num == "2")
-    (
-        Console.Clear();
-    )
+    {
+        Usuario usuario = new Usuario();
+
+        Console.WriteLine("digite o seu nome: ");
+        usuario.Nome = Console.ReadLine();
+        
+        Console.WriteLine("digite a sua senha: ");
+        usuario.Senha = Console.ReadLine();
+
+    }
     else if (num == "3")
-    (
+    {
        break;
-    )
-    else
-    (
+    }
+   else
+    {
         Console.Clear();
         Console.WriteLine("Operação invalida");
-    )
-)
+    }
+}
